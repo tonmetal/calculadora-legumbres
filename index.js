79b3e80c1d43d398d0abb77634f911ac4ae2b5e3
Tonmetal <tonvildia@gmail.com>
2025-07-17 18:19:00 +0200

    mejora de uso

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let input = document.getElementById("input")
 let boton = document.getElementById("boton")
 
-boton.addEventListener("click", function(){
+input.addEventListener("input", function(){
     if(input.value > 2000 && input.value < 3000){
-    input.value = ((input.value - 1450)/ 2 + 1450)
+    let resultado =  ((input.value - 1450)/ 2 + 1450)
+    boton.textContent = resultado 
     }
 })
